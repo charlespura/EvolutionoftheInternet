d7a144c38f5747313f1be310463e67ffddae19d4
Charles Pura <charles051902pura@gmail.com>
2026-07-10 15:45:23 +0800

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.NODE_ENV === 'production' 
-    ? '/EvolutionoftheInternet/' 
-    : '/',
+  base: '/EvolutionoftheInternet/',
   optimizeDeps: {
     include: ['three'],
   },
